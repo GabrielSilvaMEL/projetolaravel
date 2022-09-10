f670d35a5e73b8b8ef0d8b1b0788d36e15576a52
GabrielSilvaMEL <gabrielsilvaalves3000@outlook.com>
2022-09-10 12:29:35 -0300

    update controllers

--- project02/app/Http/Controllers/ContatosController.php
+++ project02/app/Http/Controllers/ContatosController.php
@@ -7,5 +7,7 @@ use Illuminate\Http\Request;
 
 class ContatosController extends Controller
 {
-    //
+    public function index(){
+        return view('site.contatos');
+    }
 }
--- project02/routes/web.php
+++ project02/routes/web.php
@@ -13,12 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('site.home');
-})->name('site.home');
-Route::get('/cursos', function () {
-    return view('site.cursos');
-})->name('site.curso');
-Route::get('/contatos', function () {
-    return view('site.contatos');
-})->name('site.contatos');
+// Route::get('/', function () {
+//     return view('site.home');
+// })->name('site.home');
+// Route::get('/cursos', function () {
+//     return view('site.cursos');
+// })->name('site.curso');
+// Route::get('/contatos', function () {
+//     return view('site.contatos');
+// })->name('site.contatos');
+
+Route::get('/',[HomeController::class,'index'])->name('site.home');
+
+Route::get('/cursos', [CursosController::class, 'index'])->name('site.cursos');
+
+Route::get('/contatos', [ContatosController::class, 'index'])->name('site.contatos');
+Route::post('/contatos', [ContatosController::class, 'index'])->name('site.contatos');
